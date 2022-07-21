ab3369ed8dfe2b40f1e3af8288dac756ef0e392d
Ahra Cho <choar816@postech.ac.kr>
2022-07-21 14:54:42 +0900

    ch1 추가

---
+++ ch1/src/scenes/PlayingScene.js
@@ -0,0 +1,39 @@
+import Phaser from "phaser";
+import Config from "../Config";
+import Player from "../characters/Player";
+import { setBackground } from "../utils/backgroundManager";
+
+export default class PlayingScene extends Phaser.Scene {
+  constructor() {
+    super("playGame");
+  }
+
+  create() {
+    // 사용할 sound들을 추가해놓는 부분
+    // load는 전역적으로 어떤 scene에서든 asset을 사용할 수 있도록 load 해주는 것이고,
+    // add는 해당 scene에서 사용할 수 있도록 scene의 멤버 변수로 추가할 때 사용하는 것입니다.
+    this.sound.pauseOnBlur = false;
+    this.m_beamSound = this.sound.add("audio_beam");
+    this.m_scratchSound = this.sound.add("audio_scratch");
+    this.m_hitMobSound = this.sound.add("audio_hitMob");
+    this.m_growlSound = this.sound.add("audio_growl");
+    this.m_explosionSound = this.sound.add("audio_explosion");
+    this.m_expUpSound = this.sound.add("audio_expUp");
+    this.m_hurtSound = this.sound.add("audio_hurt");
+    this.m_nextLevelSound = this.sound.add("audio_nextLevel");
+    this.m_gameOverSound = this.sound.add("audio_gameOver");
+    this.m_gameClearSound = this.sound.add("audio_gameClear");
+    this.m_pauseInSound = this.sound.add("audio_pauseIn");
+    this.m_pauseOutSound = this.sound.add("audio_pauseOut");
+
+    // player를 추가합니다.
+    this.m_player = new Player(this);
+
+    // background를 설정합니다.
+    setBackground(this, "background1");
+  }
+
+  update() {
+
+  }
+}
