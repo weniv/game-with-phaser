6c27fcffda144f1eab541858f693a494b9610e94
Ahra Cho <choar816@postech.ac.kr>
2022-07-26 22:50:32 +0900

    ch6 추가

--- ch6/src/characters/Player.js
+++ ch6/src/characters/Player.js
@@ -1,5 +1,6 @@
 import Phaser from "phaser";
 import Config from "../Config";
+import HpBar from "../ui/HpBar";
 
 export default class Player extends Phaser.Physics.Arcade.Sprite {
   static PLAYER_SPEED = 3;
@@ -12,6 +13,10 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
     this.scale = 2;
     this.m_moving = false;
     this.canBeAttacked = true;
+
+    // HP bar를 player의 멤버 변수로 추가해줍니다.
+    this.m_hpBar = new HpBar(scene, this, 100);
+    
     this.setDepth(20);
     this.setBodySize(28, 32);
   }
@@ -21,6 +26,13 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
 
     this.scene.m_hurtSound.play();
     this.getCooldown();
+    // mob과 접촉했을 때 damage만큼 HP를 감소시켜줍니다.
+    this.m_hpBar.decrease(damage);
+
+    // HP가 0이 되면 게임오버! (다음 챕터에서 수정할 것입니다.)
+    if (this.m_hpBar.m_currentHp <= 0) {
+      console.log('GAME OVER');
+    }
   }
 
   getCooldown() {
